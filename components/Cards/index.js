bd9352d723c535be6276d0e64803b581ec5a3ff0
Seth Nadu <seth.nadu@gmail.com>
2019-07-12 12:13:10 -0400

    working on tabs

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,70 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cards =  document.querySelector(".cards-container");
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+
+.then(item => {
+    const javascriptHeadline = item.data.articles.javascript.headline;
+    const javascriptauthorPhote= item.data.articles.javascript.authorPhoto;
+    const javascriptauthorName = item.data.articles.javascript.authorName;
+    const bootstrapHeadline = item.data.articles.bootstrap.headline;
+    const bootstrapauthorPhote= item.data.articles.bootstrap.authorPhoto;
+    const bootstrapauthorName = item.data.articles.bootstrap.authorName;
+    const technologyHeadline = item.data.articles.technology.headline;
+    const technologyauthorPhote= item.data.articles.technology.authorPhoto;
+    const technologyauthorName = item.data.articles.technology.authorName;
+    const jqueryHeadline = item.data.articles.jquery.headline;
+    const jqueryauthorPhote= item.data.articles.jquery.authorPhoto;
+    const jqueryauthorName = item.data.articles.jquery.authorName;
+    const nodeHeadline = item.data.articles.node.headline;
+    const nodeauthorPhote= item.data.articles.node.authorPhoto;
+    const nodeauthorName = item.data.articles.node.authorName;
+
+    const javaScriptCard = createCard(javascriptHeadline, javascriptauthorPhote, javascriptauthorName)
+    const bootstrapCard = createCard(bootstrapHeadline, bootstrapauthorPhote, bootstrapauthorName)
+    const technologyCard = createCard(technologyHeadline, technologyauthorPhote, technologyauthorName)
+    const jqueryCard = createCard(jqueryHeadline, jqueryauthorPhote, jqueryauthorName);
+    const nodeCard = createCard(nodeHeadline, nodeauthorPhote, nodeauthorName)
+
+
+    cards.appendChild(javaScriptCard);
+    cards.appendChild(bootstrapCard);
+    cards.appendChild(technologyCard);
+    cards.appendChild(jqueryCard);
+    cards.appendChild(nodeCard);
+
+})
+
+
+
+
+
+function createCard (headlineArticle, imgSrc, authorsNameArticle) {
+    const card = document.createElement("div")
+    const headline = document.createElement("div")
+    const author = document.createElement("div")
+    const imgDiv = document.createElement("div")
+    const img = document.createElement("img")
+    const authorsName = document.createElement("span")
+
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgDiv.classList.add("img-container");
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgDiv);
+    imgDiv.appendChild(img);
+    author.appendChild(authorsName);
+
+    headline.textContent = headlineArticle;
+    img.src = imgSrc;
+    authorsName.textContent = authorsNameArticle;
+
+    return card;
+}
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,39 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(data => {
+ const javascript  = data.data.topics[0];
+ const bootstrap  = data.data.topics[1];
+ const technology  = data.data.topics[2];
+ const jquery  = data.data.topics[3];
+ const node  = data.data.topics[4];
+ 
+ const info = tabBar(javascript, bootstrap, technology, jquery, node);
+
+ topics.appendChild(info);
+
+}) 
+
+.catch(error => {
+    console.log("There was an error fetching data", error)
+})
+
+const topics = document.querySelector(".topics");
+
+
+
+function tabBar(topic) {
+    const topicDiv = document.createElement("div");
+
+    topicDiv.classList.add("tab");
+
+    topicDiv.textContent = topic;
+    
+
+    return topicDiv
+}
